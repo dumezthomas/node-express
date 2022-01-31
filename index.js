dd0f0284b667e1631eafc79183d7c9a5f4b9aa55
Thomas Dumez <77916268+dumezthomas@users.noreply.github.com>
2022-01-30 17:22:56 -0800

    Express Simple REST

--- index.js
+++ index.js
@@ -1,18 +1,75 @@
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.all("/dishes", (req, res, next) => {
+  res.statusCode = 200; // OK
+  res.setHeader("Content-Type", "text/plain");
+  next();
+});
+
+// dishes /////////////////////////////////////////////////////////////////////
+
+app.get("/dishes", (req, res, next) => {
+  res.end("Will send all the dishes to you!");
+});
+
+app.post("/dishes", (req, res, next) => {
+  res.end(
+    "Will add the dish: " +
+      req.body.name +
+      " with details: " +
+      req.body.description
+  );
+});
+
+app.put("/dishes", (req, res, next) => {
+  res.statusCode = 403; // Not supported
+  res.end("PUT operation not supported on /dishes");
+});
+
+app.delete("/dishes", (req, res, next) => {
+  res.end("Deleting all the dishes!");
+});
+
+// dishes/:dishId /////////////////////////////////////////////////////////////
+
+app.get("/dishes/:dishId", (req, res, next) => {
+  res.end("Will send details of the dish: " + req.params.dishId + " to you!");
+});
+
+app.post("/dishes/:dishId", (req, res, next) => {
+  res.statusCode = 403; // Not supported
+  res.end("POST operation not supported on /dishes/" + req.params.dishId);
+});
+
+app.put("/dishes/:dishId", (req, res, next) => {
+  res.write("Updating the dish: " + req.params.dishId);
+  res.end(
+    "Will update the dish: " +
+      req.body.name +
+      " with details: " +
+      req.body.description
+  );
+});
+
+app.delete("/dishes/:dishId", (req, res, next) => {
+  res.end("Deleting the dish: " + req.params.dishId);
+});
 
 app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text-html");
+  res.statusCode = 200; // OK
+  res.setHeader("Content-Type", "text/html ");
   res.end("<html><body><h1>This is an Express Server</h1></body></html>");
 });
 
