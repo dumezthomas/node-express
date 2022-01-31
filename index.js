73b893762aa7ac3af3449f5fbdd3bcdddff758d3
Thomas Dumez <77916268+dumezthomas@users.noreply.github.com>
2022-01-30 17:50:05 -0800

    Express Router

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
+const dishRouter = require("./routes/dishRouter");
+
 const hostname = "localhost";
 const port = 3000;
 
@@ -10,35 +12,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-app.all("/dishes", (req, res, next) => {
-  res.statusCode = 200; // OK
-  res.setHeader("Content-Type", "text/plain");
-  next();
-});
-
-// dishes /////////////////////////////////////////////////////////////////////
-
-app.get("/dishes", (req, res, next) => {
-  res.end("Will send all the dishes to you!");
-});
-
-app.post("/dishes", (req, res, next) => {
-  res.end(
-    "Will add the dish: " +
-      req.body.name +
-      " with details: " +
-      req.body.description
-  );
-});
-
-app.put("/dishes", (req, res, next) => {
-  res.statusCode = 403; // Not supported
-  res.end("PUT operation not supported on /dishes");
-});
-
-app.delete("/dishes", (req, res, next) => {
-  res.end("Deleting all the dishes!");
-});
+app.use("/dishes", dishRouter);
 
 // dishes/:dishId /////////////////////////////////////////////////////////////
 
@@ -52,7 +26,7 @@ app.post("/dishes/:dishId", (req, res, next) => {
 });
 
 app.put("/dishes/:dishId", (req, res, next) => {
-  res.write("Updating the dish: " + req.params.dishId);
+  res.write("Updating the dish: " + req.params.dishId + "\n");
   res.end(
     "Will update the dish: " +
       req.body.name +
